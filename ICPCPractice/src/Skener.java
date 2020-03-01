33e353f8b323a6c00d6d83ca274c4c70be1dd88e
Lam Nguyen <lnguye16@gustavus.edu>
2020-02-29 21:58:07 -0600

    Add new problem

---
+++ ICPCPractice/src/Skener.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Skener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int r = scan.nextInt();
+		int c = scan.nextInt();
+		int ZR = scan.nextInt();
+		int ZC = scan.nextInt();
+		String[][] in = new String[r][c];
+		for (int i = 0; i < r; i++) {
+			String n = scan.next();
+			for (int j = 0; j < c; j++) {
+				String cha = Character.toString(n.charAt(j));
+				in[i][j] = cha;
+			}
+		}
+		for(int i = 0; i<in.length;i++) {
+			for(int j = 0; j < ZR; j++) {
+				for(int k = 0; k<in[0].length;k++) {
+					for(int g = 0;g<ZC;g++) {
+						System.out.print(in[i][k]);
+					}
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+}
